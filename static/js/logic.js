09253f94109c334b4bfd361f3c9ede0623034d68
Panagioti (Pete) Tsivis <87956455+Greekman12490@users.noreply.github.com>
2021-11-07 15:03:04 -0500

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,12 +2,10 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+let map = L.map("mapid", {center: [40.7, -94.5],zoom: 4});
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -18,5 +16,3 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-
-
